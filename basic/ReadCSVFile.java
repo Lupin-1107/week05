8d77d698efa33d218bcae44ceab850d3771fb8b4
Lupin_1107 <amitkumar104203@gmail.com>
2025-02-18 23:21:52 +0530

    [Amit] Add. Read a CSV File and Print Data

---
+++ basic/ReadCSVFile.java
@@ -0,0 +1,31 @@
+package com.tit.day1_csv.basic;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadCSVFile {
+
+    public static void main(String[] args) {
+        String filePath = "read.csv";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+
+            boolean isHeader = true;
+
+            String line;
+            System.out.printf("%-5s %-7s %-5s %-5s%n","ID","Name","Age","Marks");
+            while((line = br.readLine())!= null){
+                if(isHeader){
+                    isHeader = false;
+                    continue;
+                }
+                String [] data = line.split(",");
+                System.out.printf("%-5s %-7s %-5s %-5s%n",data[0],data[1],data[2],data[3]);
+            }
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
